cc7d30559e0598ec781d12dccc4c5a97b12ece7e
Dabbiks <mikolajmarcingrotek@gmail.com>
2026-04-22 13:26:28 +0200

    Symbol utils

---
+++ src/main/java/dabbiks/crockpot/utils/SymbolUtils.java
@@ -0,0 +1,15 @@
+package dabbiks.crockpot.utils;
+
+public class SymbolUtils {
+
+    // * BUTTONS
+    public final String SHIFT = "§f\uE080";
+    public final String MOUSE_LEFT = "§f\uE07E";
+    public final String MOUSE_RIGHT = "§f\uE07F";
+
+    // * WEATHER
+    public final String WEATHER_STORM = "§f\uE083";
+    public final String WEATHER_SUNNY = "§f\uE081";
+    public final String WEATHER_RAINY = "§f\uE082";
+
+}
