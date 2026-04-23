4156bbbf5eec825687f5c61a2e1d228ae5a312e0
Dabbiks <mikolajmarcingrotek@gmail.com>
2026-04-23 15:59:33 +0200

    Grid manager

---
+++ src/main/java/dabbiks/crockpot/managers/world/WorldGridManager.java
@@ -0,0 +1,28 @@
+package dabbiks.crockpot.managers.world;
+
+import org.bukkit.Bukkit;
+
+public class WorldGridManager {
+
+    private final int gridSize = 5;
+    private final boolean[][] grid = new boolean[gridSize][gridSize];
+
+    public int[] claimLocation() {
+        for (int row = 0; row < gridSize; row++) {
+            for (int col = 0; col < gridSize; col++) {
+                if (!grid[row][col]) {
+                    grid[row][col] = true;
+                    return new int[]{row, col};
+                }
+            }
+        }
+        Bukkit.getLogger().severe("[RESTAURANT] Failed to find empty grid location");
+        return null;
+    }
+
+    public void releaseLocation(int row, int col) {
+        if (row >= 0 && row < gridSize && col >= 0 && col < gridSize) {
+            grid[row][col] = false;
+        }
+    }
+}
